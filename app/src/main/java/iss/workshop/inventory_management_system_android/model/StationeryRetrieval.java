b73904b046bc971b74bee5b988a371f0a2da7b47
Arju5 <arjun.chirumalle@gmail.com>
2020-08-29 16:32:13 +0800

    getters and setters

--- app/src/main/java/iss/workshop/inventory_management_system_android/model/StationeryRetrieval.java
+++ app/src/main/java/iss/workshop/inventory_management_system_android/model/StationeryRetrieval.java
@@ -11,5 +11,75 @@ public class StationeryRetrieval {
     public String srRetrievalDate;
     public String srAssignedDate;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Employee getStoreClerk() {
+        return storeClerk;
+    }
+
+    public void setStoreClerk(Employee storeClerk) {
+        this.storeClerk = storeClerk;
+    }
+
+    public Employee getWarehousePacker() {
+        return warehousePacker;
+    }
+
+    public void setWarehousePacker(Employee warehousePacker) {
+        this.warehousePacker = warehousePacker;
+    }
+
+    public String getSrCode() {
+        return srCode;
+    }
+
+    public void setSrCode(String srCode) {
+        this.srCode = srCode;
+    }
+
+    public int getSrStatus() {
+        return srStatus;
+    }
+
+    public void setSrStatus(int srStatus) {
+        this.srStatus = srStatus;
+    }
+
+    public String getSrComments() {
+        return srComments;
+    }
+
+    public void setSrComments(String srComments) {
+        this.srComments = srComments;
+    }
+
+    public String getSrDate() {
+        return srDate;
+    }
+
+    public void setSrDate(String srDate) {
+        this.srDate = srDate;
+    }
+
+    public String getSrRetrievalDate() {
+        return srRetrievalDate;
+    }
+
+    public void setSrRetrievalDate(String srRetrievalDate) {
+        this.srRetrievalDate = srRetrievalDate;
+    }
+
+    public String getSrAssignedDate() {
+        return srAssignedDate;
+    }
+
+    public void setSrAssignedDate(String srAssignedDate) {
+        this.srAssignedDate = srAssignedDate;
+    }
 }
