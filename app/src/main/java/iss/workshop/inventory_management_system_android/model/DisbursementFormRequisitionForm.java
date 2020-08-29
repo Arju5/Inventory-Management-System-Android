1c4f5a39462edaf0995f0b7b97b08232ea4f4bd4
Arju5 <arjun.chirumalle@gmail.com>
2020-08-29 15:51:00 +0800

    Create Requisition for Employee 2

---
+++ app/src/main/java/iss/workshop/inventory_management_system_android/model/DisbursementFormRequisitionForm.java
@@ -0,0 +1,8 @@
+package iss.workshop.inventory_management_system_android.model;
+
+public class DisbursementFormRequisitionForm {
+    public int id;
+    public DisbursementForm disbursementForm;
+    public RequisitionForm requisitionForm;
+    public int dfrfStatus;
+}
---
+++ app/src/main/java/iss/workshop/inventory_management_system_android/model/StationeryRetrieval.java
@@ -0,0 +1,15 @@
+package iss.workshop.inventory_management_system_android.model;
+
+public class StationeryRetrieval {
+    public int id;
+    public Employee storeClerk;
+    public Employee warehousePacker;
+    public String srCode;
+    public int srStatus;
+    public String srComments;
+    public String srDate;
+    public String srRetrievalDate;
+    public String srAssignedDate;
+
+
+}
